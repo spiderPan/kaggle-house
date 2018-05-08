15d9daa6f3ce510e65fee0a2820a04653ad56482
pan <panbanglanfeng@gmail.com>
2018-05-07 23:48:30 -0400

    load data and drop Id

--- run.py
+++ run.py
@@ -22,6 +22,19 @@ warnings.warn = ignore_warn
 pd.set_option('display.float_format', lambda x: '{:.3f}'.format(x))
 
 
-print(check_output(['ls', './data']).decode('utf8'))
+# print(check_output(['ls', './data']).decode('utf8'))
 
-# dataset = pd.read_csv('data/test.csv')
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+
+print("The train data size before dropping Id feature is : {} ".format(train.shape))
+print("The test data size before dropping Id feature is : {} ".format(test.shape))
+
+train_ID = train['Id']
+test_ID = test['Id']
+
+train.drop('Id', axis=1, inplace=True)
+test.drop('Id', axis=1, inplace=True)
+
+print("\nThe train data size after dropping Id feature is : {} ".format(train.shape))
+print("The test data size after dropping Id feature is : {} ".format(test.shape))
