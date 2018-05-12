266373a1447dd74da3f7e94a791adc4cb8b32e74
pan <panbanglanfeng@gmail.com>
2018-05-12 17:26:24 -0400

    take care of missing values

--- run.py
+++ run.py
@@ -42,7 +42,7 @@ fig, ax = plt.subplots()
 ax.scatter(x=train['GrLivArea'], y=train['SalePrice'])
 plt.ylabel('SalePrice')
 plt.xlabel('GrLivArea')
-plt.show()
+# plt.show()
 
 train = train.drop(train[(train['GrLivArea'] > 4000) & (train['SalePrice'] < 300000)].index)
 
@@ -50,7 +50,7 @@ fig, ax = plt.subplots()
 ax.scatter(x=train['GrLivArea'], y=train['SalePrice'])
 plt.ylabel('SalePrice')
 plt.xlabel('GrLivArea')
-#plt.show()
+# plt.show()
 
 sns.distplot(train['SalePrice'], fit=norm)
 (mu, sigma) = norm.fit(train['SalePrice'])
@@ -62,7 +62,7 @@ plt.title('SalePrice distribution')
 
 fig = plt.figure()
 res = stats.probplot(train['SalePrice'], plot=plt)
-#plt.show()
+# plt.show()
 
 # Features Engineering
 train['SalePrice'] = np.log1p(train['SalePrice'])
@@ -74,10 +74,11 @@ all_data.drop(['SalePrice'], axis=1, inplace=True)
 print('all_data size is : {}'.format(all_data.shape))
 
 # Missing Data
+# Check missing values
 all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
 all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
 missing_data = pd.DataFrame({'Missing Ratio': all_data_na})
-missing_data.head(20)
+print(missing_data)
 
 f, ax = plt.subplots(figsize=(15, 12))
 plt.xticks(rotation='90')
@@ -86,8 +87,39 @@ plt.xlabel('Features', fontsize=15)
 plt.ylabel('Percent of missing values', fontsize=15)
 plt.title('Percent missing data by feature', fontsize=15)
 
+# Fill missing values based on data description
+none_na_features = ['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu',
+                    'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond',
+                    'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1',
+                    'BsmtFinType2', 'MasVnrType', 'MSSubClass']
+zero_na_features = ['GarageYrBlt', 'GarageArea', 'GarageCars', 'BsmtFinSF1',
+                    'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath',
+                    'BsmtHalfBath', 'MasVnrArea']
+common_na_features = ['MSZoning', 'Electrical', 'KitchenQual', 'Exterior1st',
+                      'Exterior2nd', 'SaleType']
+
+drop_features = ['Utilities']
+
+for feature in none_na_features:
+    all_data[feature] = all_data[feature].fillna('None')
+
+for feature in zero_na_features:
+    all_data[feature] = all_data[feature].fillna(0)
+
+for feature in common_na_features:
+    all_data[feature] = all_data[feature].fillna(all_data[feature].mode()[0])
+
+all_data = all_data.drop(drop_features, axis=1)
+all_data["Functional"] = all_data["Functional"].fillna("Typ")
+all_data['LotFrontage'] = all_data.groupby('Neighborhood')['LotFrontage'].transform(lambda x: x.fillna(x.median()))
+
+
+all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+missing_data = pd.DataFrame({'Missing Ratio': all_data_na})
+print(missing_data)
 # Data correlation
 corrmat = train.corr()
 plt.subplots(figsize=(12, 9))
 sns.heatmap(corrmat, vmax=0.9, square=True)
-#plt.show()
+# plt.show()
